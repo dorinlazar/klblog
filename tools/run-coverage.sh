44a1a73330af359ef16d8ab369507498fd27fa9f
Dorin Lazăr (Fedora WSL2) <dorin.lazar@gmail.com>
2023-04-30 10:47:58 +0300

    coverage added

--- tools/run-coverage.sh
+++ tools/run-coverage.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+rm -rf build/
 cmake --preset coverage
 cmake --build --preset coverage
-
+mkdir build/coverage
+lcov --capture --no-external --initial --directory . -o build/coverage/zero-coverage.info
+build/test/kl/kltests
+lcov --capture --no-external --directory .  --output-file build/coverage/kltests-coverage.info 
+lcov --add-tracefile build/coverage/zero-coverage.info --add-tracefile build/coverage/kltests-coverage.info --output-file build/coverage/full-coverage.info
+lcov --remove build/coverage/full-coverage.info "${PWD}/test/*" --output-file build/coverage/coverage.info
+genhtml build/coverage/coverage.info --output-directory build/coverage/
