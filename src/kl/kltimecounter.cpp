49af74cb56ddf06687b8d91ebf732cc5b4a57e5a
Dorin Lazăr (Fedora WSL2) <dorin.lazar@gmail.com>
2023-10-16 19:16:45 +0300

    add kl as submodule

--- src/kl/kltimecounter.cpp
+++
@@ -1,6 +0,0 @@
-#include "kltimecounter.hpp"
-
-namespace kl {
-TimeCounter::TimeCounter(const kl::Text& op) : m_start(kl::DateTime::now()), m_op(op) {}
-TimeCounter::~TimeCounter() { kl::log("{} done. Time spent: {}", m_op, kl::DateTime::now() - m_start); }
-} // namespace kl
